34e2d3ed2373dc90174e292b159b6f136b4020b9
Yen_tree <codpdms@naver.com>
2022-01-15 13:39:05 +0900

    solved boj_11727

--- 11727_2xn타일링2.py
+++ 11727_2xn타일링2.py
@@ -3,22 +3,15 @@ input = sys.stdin.readline
 
 n = int(input())
 
-# memo = [0] * (n+1)
-# memo[1], memo[2] = 1, 2
-#
-# for i in range(3, n+1):  # 상향식
-#     memo[i] = memo[i-1] + memo[i-2]
-#
-# print(memo[n]%10007)
-
-if n < 3:
-    print(n)
+a, b = 1, 3
+if n == 1:
+    print(a)
+elif n == 2:
+    print(b)
 else:
-    a, b = 1, 2
     for _ in range(3, n+1):
-        tmp = a + b
+        tmp = 2*a + b
         a = b
         b = tmp
 
     print(b % 10007)
-
