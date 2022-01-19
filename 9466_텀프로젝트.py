9e171b04f079af5c13f71b5fefc1a2df8fd2812a
devyen <codpdms@naver.com>
2022-01-20 00:01:12 +0900

    Solving: boj_9466

--- 9466_텀프로젝트.py
+++ 9466_텀프로젝트.py
@@ -1,45 +1,26 @@
 import sys
-from collections import deque
-import copy
 input = sys.stdin.readline
 
+t = int(input())
+for _ in range(t):
+    n = int(input())
+    picks = [0] + list(map(int, input().split()))
 
-def bfs():
-    tomatos = 0
-    q = deque()
-    for r in range(n):
-        for c in range(m):
-            if matrix[r][c] == 1:
-                q.append((r, c))  # 익은 토마토 큐에 담기
-            elif matrix[r][c] == 0:
-                tomatos += 1  # 안 익은 토마토 카운팅
+    rst = n
+    check = [0] * (n+1)
+    for i in range(1, n+1):
+        if not check[i]:
+            # 한 텀
+            students = []
+            while True:
+                if check[i]:  # 이미 확인한 번호면 pass
+                    break
+                students.append(i)
+                check[i] = 1
+                i = picks[i]
+                if i in students:
+                    cut = students.index(i)
+                    rst -= len(students[cut:])
+                    break
 
-    if tomatos == 0:  # 안 익은 토마토가 없으면 0 리턴
-        return 0
-
-    day = 0
-    cnt = 0
-    while q:
-        nq = copy.deepcopy(q)  # day를 세야하므로 하루치 큐를 만든다.
-        q = deque()
-        while nq:
-            i, j = nq.popleft()
-            for d in ((0, 1), (1, 0), (0, -1), (-1, 0)):  # 델타탐색
-                    ni, nj = i + d[0], j + d[1]
-                    if 0 <= ni < n and 0 <= nj < m and matrix[ni][nj] == 0:
-                        cnt += 1
-                        matrix[ni][nj] = 1
-                        q.append((ni, nj))
-        day += 1
-        
-        if cnt == tomatos:
-            return day
-
-    if cnt < tomatos:
-        return -1
-
-
-m, n = map(int, input().split())
-matrix = [list(map(int, input().split())) for _ in range(n)]
-
-print(bfs())
+    print(rst)
