4fe3193baf0a0923f8fb9d3ff76aca4e338d2f2e
YeEun <codpdms@naver.com>
2022-03-08 17:34:04 +0900

    Solve: boj_1520

--- BOJ/1520_내리막길.py
+++ BOJ/1520_내리막길.py
@@ -1,21 +1,34 @@
 import sys
+sys.setrecursionlimit(10 ** 8)
 input = sys.stdin.readline
+
+'''
+idea
+dfs랑 dp를 동시에 이용
+그래프를 도착점부터 탐색한다.
+'''
+
+
+def dfs(i, j):
+    if i == m-1 and j == n-1:
+        return 1
+
+    if dp[i][j] != -1:  # 이미 방문했으면
+        return dp[i][j]
+
+    dp[i][j] = 0
+    for di, dj in DIRECTION:
+        ni, nj = i + di, j + dj
+        if 0 <= ni < m and 0 <= nj < n and matrix[ni][nj] < matrix[i][j]:
+            dp[i][j] += dfs(ni, nj)
+
+    return dp[i][j]
+
 
 DIRECTION = ((0, 1), (1, 0), (0, -1), (-1, 0))
 
 m, n = map(int, input().split())
 matrix = [list(map(int, input().split())) for _ in range(m)]
-memo = [[0]*n for _ in range(m)]
-memo[0][0] = 1
+dp = [[-1] * n for _ in range(m)]
 
-result = 0
-for i in range(n):
-    for j in range(m):
-        if memo[i][j]:
-           for di, dj in DIRECTION:
-               if 0 <= i + di < n and 0 <= j + dj < n:
-                   
-
-
-print(result)
-
+print(dfs(0, 0))
