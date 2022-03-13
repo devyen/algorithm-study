9e3a250f256a2d5721386141d28fe95a6d731875
YeEun <codpdms@naver.com>
2022-03-13 14:57:23 +0900

    Solve: boj_1992

--- BOJ/1992_쿼드트리.py
+++ BOJ/1992_쿼드트리.py
@@ -9,10 +9,7 @@ idea
 
 def quad_tree(i, j, k):  # i, j: 영역의 좌상단 좌표, k: 한변의 길이
     if k == 1:
-        if matrix[i][j]:
-            return '1'
-        else:
-            return '0'
+        return matrix[i][j]  # '1' or '0'
 
     a = quad_tree(i, j, k//2)  # 왼쪽 위
     b = quad_tree(i, j+k//2, k//2)  # 오른쪽 위
@@ -22,18 +19,13 @@ def quad_tree(i, j, k):  # i, j: 영역의 좌상단 좌표, k: 한변의 길이
     if a == b == c == d:
         if a == '1':
             return '1'
-        else:
+        elif a == '0':
             return '0'
 
     return '(' + a + b + c + d + ')'
 
 
 n = int(input())
-matrix = [list(map(int, input().rstrip())) for _ in range(n)]
+matrix = [list(input().strip()) for _ in range(n)]  # 문자열로 된 matrix
 
-result = quad_tree(0, 0, n)
-if result[0] != '(':
-    result = '(' + result + ')'
-print(result)
-
-
+print(quad_tree(0, 0, n))
