723634f2ad33932d9eeceffd466edce70c8f6305
YeEun <codpdms@naver.com>
2022-04-14 20:24:54 +0900

    Solve: pro_신고결과받기2

--- Programmers/Level1_신고결과받기_2.py
+++ Programmers/Level1_신고결과받기_2.py
@@ -1,16 +1,15 @@
 def solution(id_list, report, k):
     n = len(id_list)
     ids = {}
-    cnt = 0
-    for id in id_list:
-        ids[id] = cnt
-        cnt += 1
+
+    for i, id in enumerate(id_list):
+        ids[id] = i
 
     report_cnt = [set([]) for _ in range(n)]  # set으로 중복 제거
     for r in report:
         a, b = r.split()
         a, b = ids[a], ids[b]  # 닉네임을 번호로 변경
-        report_cnt[b].append(a)
+        report_cnt[b].add(a)
 
     answer = [0] * n
     for reporters in report_cnt:
@@ -21,5 +20,5 @@ def solution(id_list, report, k):
     return answer
 
 
-a = solution(["muzi", "frodo", "apeach", "neo"], ["muzi frodo","apeach frodo","frodo neo","muzi neo","apeach muzi"], 2)
-print(a)
+result = solution(["muzi", "frodo", "apeach", "neo"], ["muzi frodo","apeach frodo","frodo neo","muzi neo","apeach muzi"], 2)
+print(result)
