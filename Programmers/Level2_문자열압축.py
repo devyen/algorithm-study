a8a65fa007ceaa2e1de06f76a8addd732ed5f4bb
YeEun <codpdms@naver.com>
2022-04-19 21:29:28 +0900

    Solve: 문자열압축

--- Programmers/Level2_문자열압축.py
+++ Programmers/Level2_문자열압축.py
@@ -1,29 +1,38 @@
 def solution(s):
     n = len(s)
+    if n == 1:
+        return 1
     answer = float('inf')
     for k in range(1, n//2+1):
         result = ''
         pattern = s[0:k]
         i, cnt, flag = k, 1, 0
         while i < n:
+            if len(result) >= answer:  # 가지치기
+                break
             if n-i < k:  # next 길이가 모자란 경우 -> 이전 패턴 & 남은 부분을 result에 이어붙인다
-                result += str(cnt) + pattern + s[i:]
+                if cnt > 1:
+                    result += str(cnt)
+                result += pattern + s[i:]
                 flag = 1
                 break
             next = s[i:i+k]
             if next == pattern:
                 cnt += 1
             else:
-                result += str(cnt) + pattern
+                if cnt > 1:
+                    result += str(cnt)
+                result += pattern
                 pattern = next
                 cnt = 1
             i += k
         if not flag:  # 마지막 패턴 이어붙이기
-            result += str(cnt) + pattern
-        result = result.replace('1', '')
+            if cnt > 1:
+                result += str(cnt)
+            result += pattern
         answer = min(answer, len(result))
     return answer
 
 
-result = solution('aabbaccc')
+result = solution('abcabcabcdabcfabcabcabczabcabcdddabc')
 print(result)
