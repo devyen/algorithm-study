522710de6671b86bab87cd5cb82f7585a1b0ffcf
YeEun <codpdms@naver.com>
2022-06-15 16:55:29 +0900

    Solve: Level2_캐시

---
+++ Programmers/Level2_캐시.py
@@ -0,0 +1,22 @@
+from collections import deque
+
+
+def solution(cacheSize, cities):
+    cache = deque([])
+    answer = 0
+    for city in cities:
+        city = city.lower()
+        if city in cache:
+            answer += 1
+            cache.remove(city)
+            cache.append(city)
+        else:
+            answer += 5
+            cache.append(city)
+            if len(cache) > cacheSize:
+                cache.popleft()
+    return answer
+
+
+result = solution(2, ["Jeju", "Pangyo", "NewYork", "newyork"])
+print(result)
